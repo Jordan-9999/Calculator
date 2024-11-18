55b4cce91cb86a6dee344cbc96200c9a9ea5d271
jordan <jsheeler90@gmail.com>
2024-11-18 17:23:37 -0500

    Created functions for the basic math operators.
    Created add, subtract, multiply and divide functions and left some tests for the console there.

---
+++ script.js
@@ -0,0 +1,13 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) =>  a * b;
+
+const division = (a, b) => a / b;
+
+
+console.log(add(4, 2));
+console.log(subtract(4, 2));
+console.log(multiply(4, 2));
+console.log(division(4, 2));
