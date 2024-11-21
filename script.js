4fc8448f235005a6e5311b2be3ae0eb8b0b54bd1
jordan <jsheeler90@gmail.com>
2024-11-21 09:58:15 -0500

    Added 3 variables and an operate function
    Added 3 variables for each part of the operation,
    and created an operate function that takes 2
    numbers and calls on one of the math operator functions.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const add = (a, b) => a + b;
+const add = (a, b) => a + b; 
 
 const subtract = (a, b) => a - b;
 
@@ -6,8 +6,20 @@ const multiply = (a, b) =>  a * b;
 
 const division = (a, b) => a / b;
 
+let numberA;
 
-console.log(add(4, 2));
-console.log(subtract(4, 2));
-console.log(multiply(4, 2));
-console.log(division(4, 2));
+let operator;
+
+let numberB;
+
+//console.log(add(4, 2));
+//console.log(subtract(4, 2));
+//console.log(multiply(4, 2));
+//console.log(division(4, 2));
+
+
+const operate = (a, operator, b) => operator(a, b);
+    
+    
+    
+console.log(operate(5, multiply, 3));
