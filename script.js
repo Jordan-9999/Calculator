b3c149931742a25a817804c0284354ef3780c73b
jordan <jsheeler90@gmail.com>
2024-11-25 13:33:57 -0500

    Created functions for the numbers and clear button
    All of the numbers now have a value assigned to them
    in the html, when the number button is clicked that
    value is added in to the input display. That added
    into value is also now stored in the numberA variable.
    The clear button now erases the input diplay.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const multiply = (a, b) =>  a * b;
 
 const division = (a, b) => a / b;
 
-let numberA;
+let numberA = "";
 
 let operator;
 
@@ -20,6 +20,66 @@ let numberB;
 
 const operate = (a, operator, b) => operator(a, b);
     
-    
-    
 console.log(operate(5, multiply, 3));
+
+let display = document.querySelector("#display");
+//display.value = 6;
+
+
+
+const button0 = document.querySelector("#button0");
+button0.addEventListener("click", function(){  
+    display.value += button0.value;
+    numberA = display.value
+});
+
+const button1 = document.querySelector("#button1");
+button1.addEventListener("click", function(){  
+    display.value += button1.value;
+    numberA = display.value
+});
+const button2 = document.querySelector("#button2");
+button2.addEventListener("click", function(){  
+    display.value += button2.value;
+    numberA = display.value
+});
+const button3 = document.querySelector("#button3");
+button3.addEventListener("click", function(){  
+    display.value += button3.value;
+    numberA = display.value
+});
+const button4 = document.querySelector("#button4");
+button4.addEventListener("click", function(){  
+    display.value += button4.value;
+    numberA = display.value
+});
+const button5 = document.querySelector("#button5");
+button5.addEventListener("click", function(){  
+    display.value += button5.value;
+    numberA = display.value
+});
+const button6 = document.querySelector("#button6");
+button6.addEventListener("click", function(){  
+    display.value += button6.value;
+    numberA = display.value
+});
+const button7 = document.querySelector("#button7");
+button7.addEventListener("click", function(){  
+    display.value += button7.value;
+    numberA = display.value
+});
+const button8 = document.querySelector("#button8");
+button8.addEventListener("click", function(){  
+    display.value += button8.value;
+    numberA = display.value
+});
+const button9 = document.querySelector("#button9");
+button9.addEventListener("click", function(){  
+    display.value += button9.value;
+    numberA = display.value
+});
+const buttonClear = document.querySelector("#button-clear");
+buttonClear.addEventListener("click", function() {
+    display.value = "";
+});
+
