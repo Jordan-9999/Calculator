bc238c5dab28ea78a1ce5f0ca876c0528b98d263
jordan <jsheeler90@gmail.com>
2024-12-16 10:37:14 -0500

    New button classes,loops through button functions
    Created a numButton class, and an operator button
    class. Created a forEach loop so there's one
    addEventListener for all the number buttons
    instead of 10 seperate ids that are all doing the
    same thing.
    Also did the same for the disabling and enabling
    button functions.

--- script.js
+++ script.js
@@ -12,17 +12,18 @@ let operator = "";
 
 let numberB = "";
 
+let numButton = document.querySelectorAll(".num-button");
+let operatorButton = document.querySelectorAll(".operator-button");
+let display = document.querySelector("#display");
+
 const errorMessageFunc = function(){
     disableNumButtons();
     disableOperators();
     return display.value = "Whoa! You can't do that!"
 };
 
-
 const operate = (a, operator, b) => numberB == 0 && operator == division ? errorMessageFunc() :
 parseFloat(operator(a, b).toFixed(8));
-
-
 
 const storeValues = function(){
     let splitUpDisplay = display.value.split(" ").map(Number);
@@ -34,100 +35,27 @@ const storeValues = function(){
 };
 
 const enableOperators = function(){
-    plusButton.disabled = false;
-    minusButton.disabled = false;
-    multiplyButton.disabled = false;
-    divideButton.disabled = false;
+    operatorButton.forEach(button => button.disabled = false);
 };
 const disableOperators = function(){
-    plusButton.disabled = true;
-    minusButton.disabled = true;
-    multiplyButton.disabled = true;
-    divideButton.disabled = true;
+    operatorButton.forEach(button => button.disabled = true);
     equalsButton.disabled = true;
 };
-
-const disableNumButtons = function(){
-    button0.disabled = true;
-    button1.disabled = true;
-    button2.disabled = true;
-    button3.disabled = true;
-    button4.disabled = true;
-    button5.disabled = true;
-    button6.disabled = true;
-    button7.disabled = true;
-    button8.disabled = true;
-    button9.disabled = true;
-};
 const enableNumButtons = function(){
-    button0.disabled = false;
-    button1.disabled = false;
-    button2.disabled = false;
-    button3.disabled = false;
-    button4.disabled = false;
-    button5.disabled = false;
-    button6.disabled = false;
-    button7.disabled = false;
-    button8.disabled = false;
-    button9.disabled = false;
-}
+    numButton.forEach(button => button.disabled = false);
+    };
+const disableNumButtons = function(){
+numButton.forEach(button => button.disabled = true);
+};
 
 
 
+numButton.forEach(button => {
+    button.addEventListener("click", ()=> {
+        display.value += button.value;
+        storeValues();});
+});
 
-
-let display = document.querySelector("#display");
-
-const button0 = document.querySelector("#button0");
-button0.addEventListener("click", function(){  
-    display.value += button0.value;
-    storeValues();
-});
-const button1 = document.querySelector("#button1");
-button1.addEventListener("click", function(){ 
-    display.value += button1.value;
-    storeValues();
-});
-const button2 = document.querySelector("#button2");
-button2.addEventListener("click", function(){  
-    display.value += button2.value;
-    storeValues();
-});
-const button3 = document.querySelector("#button3");
-button3.addEventListener("click", function(){  
-    display.value += button3.value;
-    storeValues();
-});
-const button4 = document.querySelector("#button4");
-button4.addEventListener("click", function(){  
-    display.value += button4.value;
-    storeValues();
-});
-const button5 = document.querySelector("#button5");
-button5.addEventListener("click", function(){  
-    display.value += button5.value;
-    storeValues();
-});
-const button6 = document.querySelector("#button6");
-button6.addEventListener("click", function(){  
-    display.value += button6.value;
-    storeValues();
-});
-const button7 = document.querySelector("#button7");
-button7.addEventListener("click", function(){  
-    display.value += button7.value;
-    storeValues();
-});
-const button8 = document.querySelector("#button8");
-button8.addEventListener("click", function(){  
-    display.value += button8.value;
-    storeValues();
-});
-const button9 = document.querySelector("#button9");
-button9.addEventListener("click", function(){  
-    display.value += button9.value;
-    storeValues();
-});
 
 
 const plusButton = document.querySelector("#plus-button");
