7dbb4d3fbfc389afd1c7cc194ec58aa3649753b7
jordan <jsheeler90@gmail.com>
2024-12-30 15:13:28 -0500

    Created decimal button, added enable and disable
    If numberA is empty"", a zero will be added to display
    before decimal, when any operator button is clicked,
    numberA is set to empty"" again, in case 2nd part
    of equation starts with zero decimal.
    Decimal Button is disabled after clicking it.
    When an operator is clicked and disabled, decimal
    button is enabled.
    Equals button now calls storeValues so the result
    can be checked if it has a decimal or not.
    I moved the error message if statement out of the
    operate function and added it to the equals button
    so that when storeValues is called with equalsButton,
    the operator buttons won't be enabled after
    error message.

--- script.js
+++ script.js
@@ -12,18 +12,26 @@ let operator = "";
 
 let numberB = "";
 
+
+
 let numButton = document.querySelectorAll(".num-button");
 let operatorButton = document.querySelectorAll(".operator-button");
 let display = document.querySelector("#display");
 
+
+
+
 const errorMessageFunc = function(){
     disableNumButtons();
     disableOperators();
+    decimalButton.disabled = true;
     return display.value = "Whoa! You can't do that!"
 };
 
-const operate = (a, operator, b) => numberB == 0 && operator == division ? errorMessageFunc() :
-parseFloat(operator(a, b).toFixed(8));
+const operate = (a, operator, b) => parseFloat(operator(a, b).toFixed(8));
+
+
+
 
 const storeValues = function(){
     let splitUpDisplay = display.value.split(" ").map(Number);
@@ -40,6 +48,7 @@ const enableOperators = function(){
 const disableOperators = function(){
     operatorButton.forEach(button => button.disabled = true);
     equalsButton.disabled = true;
+    enableDecimal();
 };
 const enableNumButtons = function(){
     numButton.forEach(button => button.disabled = false);
@@ -47,7 +56,9 @@ const enableNumButtons = function(){
 const disableNumButtons = function(){
 numButton.forEach(button => button.disabled = true);
 };
-
+const enableDecimal = function(){
+decimalButton.disabled = false
+};
 
 
 numButton.forEach(button => {
@@ -56,6 +67,14 @@ numButton.forEach(button => {
         storeValues();});
 });
 
+const decimalButton = document.querySelector("#decimal-button");
+      decimalButton.addEventListener("click", ()=> { 
+    if(numberA === ""){display.value += 0};
+      display.value += decimalButton.value;
+      storeValues();
+      decimalButton.disabled = true;
+    });
+    
 
 
 const plusButton = document.querySelector("#plus-button");
@@ -64,15 +83,18 @@ plusButton.addEventListener("click", function(){
          operator = add;
          display.value += " + ";
          disableOperators();
+         numberA = "";
         }
         else if(numberB === 0 && operator === division){
             errorMessageFunc();
         }
     else{
         display.value = operate(numberA, operator, numberB);
+        if(numberA.toString().includes(".")){decimalButton.disabled = true};
         operator = add;
         display.value += " + ";
         disableOperators();
+        numberA = "";
     };
 });
 
@@ -82,15 +104,18 @@ minusButton.addEventListener("click", function(){
         operator = subtract;
         display.value += " - ";
         disableOperators();
+        numberA = "";
         }
         else if(numberB === 0 && operator === division){
             errorMessageFunc();
         }
    else{
        display.value = operate(numberA, operator, numberB);
+       if(numberA.toString().includes(".")){decimalButton.disabled = true};
        operator = subtract;
        display.value += " - ";
        disableOperators();
+       numberA = "";
    };
 });
 
@@ -99,15 +124,19 @@ multiplyButton.addEventListener("click", function(){
     if(numberB === undefined){
         operator = multiply;
         display.value += " x ";
-        disableOperators();}
+        disableOperators();
+        numberA = "";
+        }
         else if(numberB === 0 && operator === division){
             errorMessageFunc();
         }
    else{
        display.value = operate(numberA, operator, numberB);
+       if(numberA.toString().includes(".")){decimalButton.disabled = true};
        operator = multiply;
        display.value += " x ";
        disableOperators();
+       numberA = "";
    };
 });
 
@@ -116,22 +145,33 @@ divideButton.addEventListener("click", function(){
     if(numberB === undefined){
         operator = division;
         display.value += " % ";
-        disableOperators();}
+        disableOperators();
+        numberA = "";
+    }
         else if(numberB === 0 && operator === division){
             errorMessageFunc();
         }
    else{
        display.value = operate(numberA, operator, numberB);
+       if(numberA.toString().includes(".")){decimalButton.disabled = true};
        operator = division;
        display.value += " % ";
        disableOperators();
+       numberA = "";
    };
 });
 
 const equalsButton = document.querySelector("#equals-button");
 equalsButton.addEventListener("click", function(){
+    if(numberB === 0 && operator === division){
+        errorMessageFunc()}
+    else{
     display.value = operate(numberA, operator, numberB);
+    storeValues();
+    if(numberA.toString().includes(".")){decimalButton.disabled = true}
+    else{decimalButton.disabled = false};
     equalsButton.disabled = true;
+}
 });
 
 const buttonClear = document.querySelector("#button-clear");
